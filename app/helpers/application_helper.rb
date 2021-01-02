f7ca880bca1bc726fb82425f7b51e0196c3931a9
Joe Hale <me@joehael.io>
2021-01-02 15:10:21 -0800

    Removed duplicate helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,4 @@
 module ApplicationHelper
-        def main_image(image)
-        if image.main_image.attached?
-            image_tag image.main_image
-        else
-            image_tag "placeholder.png"
-        end
-    end
+
     
 end
